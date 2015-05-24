5133ef951d686b564d601c6e7aa82a50cba9d1b7
Jeff Mair <jeffwmair@gmail.com>
2015-05-24 19:09:34 -0400

    updated script that updates local maven repo

--- install_jar_maven.sh
+++ install_jar_maven.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mvn package
-mvn install:install-file -Dfile="target/j3dfw-1.0.jar" -DpomFile="pom.xml"
+mvn clean compile package install:install-file -Dfile="target/j3dfw-1.0.jar" -DpomFile="pom.xml"
